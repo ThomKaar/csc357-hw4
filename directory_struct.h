0b4051223857bb1ae87c5237874e395b5512e3c1
Thomkaar <thomkaar@gmail.com>
2018-02-27 14:28:27 -0800

    Made the D_Node and D_Child structs.

--- directory_struct.h
+++ directory_struct.h
@@ -6,5 +6,13 @@
 
 
 typdef struct {
-         
+   D_Child *entires;
+   struct stat* sb;
+   struct dirent* direntp;
 } D_Node;
+
+typdef struct {
+   D_Node* cur_node;
+   D_Node* next_node;
+   
+} D_Child;
