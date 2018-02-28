c75b6d33143799a09e0df61d3f9ddeefadd36cf1
Thomkaar <thomkaar@gmail.com>
2018-02-27 17:29:38 -0800

    name field matches Nico's output, mode is off by one char, but matches mode of dir???

--- archive_creation.c
+++ archive_creation.c
@@ -21,19 +21,17 @@ static void header_set_prefix(Header* header, char* prefix_name){
 }
 
 /*given: the path (aquired by getpwd()), a char array of 100, and a char array size 155*/
-static void prefix_name_split(char*path, char*d_name, char*name, char* prefix){
+static void prefix_name_split(char*path, char*name, char* prefix){
   int offset;
 
-  if(strlen(d_name) > 100){
-    offset = strlen(d_name) - 100;
-    strcpy(name, (d_name + offset));
+  if(strlen(path) > 100){
+    offset = strlen(path) - 100;
+    strcpy(name, (path + offset));
 
-    strcpy(prefix, path);
-    strncat(prefix, d_name, offset);
+    strncpy(prefix, path, offset);
   }
   else{
-    strncpy(name, d_name, strlen(d_name));
-    strcpy(prefix, path);
+    strcpy(name, path);
   }
 }
 
@@ -45,13 +43,13 @@ static void header_set_mode(Header *header, mode_t st_mode){
 
     clear_char_array(mode, 8);
     i = 0;
-    while(i < 7){
-      mode[7-i] = (char) ((st_mode % 8) + ASCII_NUM_OFFSET);
+    while(i <= 6){
+      mode[6-i] = (char) ((st_mode % 8) + ASCII_NUM_OFFSET);
       st_mode /= 8;
       i++;
     }
     mode[7] = '\0';
-    strcpy(header->mode, mode);
+    strncpy(header->mode, mode, 8);
 }
 
 
@@ -63,13 +61,13 @@ static void header_set_uid(Header *header, uid_t uid){
   clear_char_array(u,8);
 
   i = 0;
-  while(i < 7){
-    u[7-i] = (char) ((uid % 8) + ASCII_NUM_OFFSET);
+  while(i <= 6){
+    u[6-i] = (char) ((uid % 8) + ASCII_NUM_OFFSET);
     uid /= 8;
     i++;
   }
   u[7] = '\0';
-  strcpy(header->uid, u);
+  strncpy(header->uid, u,8);
 }
 
 static void header_set_gid(Header *header, gid_t gid){
@@ -79,13 +77,13 @@ static void header_set_gid(Header *header, gid_t gid){
   clear_char_array(g, 8);
 
   i = 0;
-  while(i < 7){
-    g[7-i] = (char) ((gid % 8) + ASCII_NUM_OFFSET);
+  while(i <= 6){
+    g[6-i] = (char) ((gid % 8) + ASCII_NUM_OFFSET);
     gid /= 8;
     i++;
   }
   g[7] = '\0'; /* Changed from 8 to 7 to be within range */
-  strcpy(header->gid, g);
+  strncpy(header->gid, g,8);
 }
 
 /**/
@@ -99,6 +97,7 @@ static void header_set_size(Header *header, off_t size, mode_t mode){
   i = 0;
   if(!S_ISREG(mode)){
     strcpy(header->size, header_size);
+    return;
   }
   else{
     while(i < 11){
@@ -116,8 +115,8 @@ static void header_set_mtime(Header *header, time_t timespec){
   int i;
   clear_char_array(tim, 12);
   i = 0;
-  while(i < 11){
-    tim[11-i] = (char) ((timespec % 8) + ASCII_NUM_OFFSET);
+  while(i <= 10){
+    tim[10-i] = (char) ((timespec % 8) + ASCII_NUM_OFFSET);
     timespec /= 8;
     i++;
   }
@@ -164,13 +163,15 @@ static void header_compute_chksum(Header *header, char* field){
 static void header_set_uname(Header* header, uid_t uid){
   struct passwd *pw;
   pw = getpwuid(uid);
-  strcpy(header->uname, pw->pw_name);
+  pw->pw_name[31] = '\0';
+  strncpy(header->uname, pw->pw_name, 32);
 }
 
 static void header_set_gname(Header* header, gid_t gid){
   struct group *gr;
   gr = getgrgid(gid);
-  strcpy(header->gname, gr->gr_name);
+  gr->gr_name[31] = '\0';
+  strncpy(header->gname, gr->gr_name, 32);
 }
 
 static void header_set_chksum(Header *header)
@@ -211,7 +212,7 @@ Header * create_header(char * path, struct dirent* direntp){
   strcpy(header->magic,"ustar");
   strcpy(header->version, "00");
 
-  prefix_name_split(path, direntp->d_name, name, prefix);
+  prefix_name_split(path, name, prefix);
   header_set_name(header, name);
   header_set_prefix(header, prefix);
   header_set_mode(header, sb->st_mode);
@@ -219,12 +220,11 @@ Header * create_header(char * path, struct dirent* direntp){
   header_set_gid(header, sb->st_gid);
   header_set_size(header, sb->st_size, sb->st_mode);
   header_set_mtime(header, sb->st_mtime);
-  header_set_chksum(header);
   header_set_typeflag(header, sb->st_mode);
   header_set_linkname(header, sb->st_mode, name); /*TODO is this the same name as above?*/
   header_set_uname(header, sb->st_uid);
   header_set_gname(header, sb->st_gid);
-
+  header_set_chksum(header);
   
   return header;
 }
@@ -264,11 +264,11 @@ void write_header(Header *header, int fd){
    
    write(fd,header->name, sizeof(uint8_t)*100);
    write(fd, header->mode, sizeof(uint8_t)*8);
+   write(fd, &header->chksum, sizeof(uint8_t));
    write(fd, header->uid, sizeof(uint8_t)*8);
    write(fd, header->gid, sizeof(uint8_t)*8);
    write(fd, header->size, sizeof(uint8_t)*12);
    write(fd, header->mtime, sizeof(uint8_t)*10);
-   write(fd, &header->chksum, sizeof(uint8_t));
    write(fd, &header->typeflag, sizeof(uint8_t));
    write(fd, header->linkname, sizeof(uint8_t)*100);
    write(fd, &header->magic, sizeof(uint8_t)*6);
