9072b51b87b952e1fb0f6729a9a0534eb8ed69bc
Andrew <apuleo36@gmail.com>
2018-02-27 23:27:07 -0800

    D_Node, Child_List, and D_Child store filesystem info

--- directory_struct.h
+++ directory_struct.h
@@ -6,13 +6,18 @@
 
 
 typdef struct {
-   D_Child *entries;
+   Child_List *entries;
    struct stat* sb;
-   struct dirent* direntp;
+   struct dirent* dir_ent;
 } D_Node;
+
+typdef struct {
+  D_Child* front;
+  D_Child* back;
+  int size;
+} Child_List;
 
 typdef struct {
    D_Node* node;
    D_Child* next;
-   
 } D_Child;
